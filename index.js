eebc68269770ecb567882cca40dcbf2c5528cd22
chrisenitan <enitanchris@gmail.com>
2021-04-28 07:39:38 +0200

    main routes and engine setup

--- index.js
+++ index.js
@@ -2,6 +2,31 @@
 const express = require("express")
 const app = express()
 
+//cookie parser
+const cookieParser = require("cookie-parser")
+app.use(cookieParser())
+
+//dotenv file
+require("dotenv").config();
+
+//views default
+const path = require("path")
+app.set("views", path.join(__dirname, "views"))
+
+//mustache
+app.set("view engine", "mustache")
+app.engine("mustache", require("hogan-middleware").__express)
+
+//public files
+app.use(express.static(path.join(__dirname, "public")))
+
+//express form parser
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+//routes
+const main = require("./routes/main")
+app.use("/", main)
 
 
 
---
+++ routes/main.js
@@ -0,0 +1,6 @@
+const express = require("express")
+
+const approuter = express()
+
+
+module.exports = approuter;
