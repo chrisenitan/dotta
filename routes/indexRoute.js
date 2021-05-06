33a4dac5c82ec610fc47b886ee6fbd11f1aa9eeb
chrisenitan <enitanchris@gmail.com>
2021-05-06 17:45:53 +0200

    new style and index wip

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -3,13 +3,13 @@ const express = require("express")
 const appRouter = express()
 
 appRouter.get("/", (req, res)=>{
-    res.json({
-        message: "lo"
-    })
+    res.render("index")
 })
 
 appRouter.get("/login", (req, res)=>{
-    res.render("index")
+    res.json({
+        message: "login here wip"
+    })
 })
 
 module.exports = appRouter;
