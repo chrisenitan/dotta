e5776c20959e7343d44a797a4db63e24b1fa5f48
chrisenitan <enitanchris@gmail.com>
2021-08-27 07:29:57 +0200

    update cron, addeed new currencies

--- ledger.php
+++ ledger.php
@@ -64,15 +64,9 @@ while ($rowMonths = mysqli_fetch_array($holderMonths)) {
   //only for months, if today is sub day or if month has more tha 30 days and today is 30 or more
   //(in_array($dateMonth, $oddMonths) and $date == 28) or 
   if ($frequency == "Every Month" and ($dateDay == $date or (in_array($dateMonth, $oddMonths) and $date > 30 and $todayDate >= 30))) {
-
     $logLedgerMonth = "INSERT INTO ledger (username, ref, cost, dateEntered) VALUES ('$username', '$ref', '$cost', '$todayDate')";
-
-    echo "<span style='color:red'>Logged Month for: $name : $ref </span> <br><br>";
-
     $updateMonthLog = "UPDATE subs SET lastBilled='$todayDate' WHERE ref = '$ref'";
-
-    //send email
-    sendMail("Month");
+    echo "<span style='color:red'>Logged Month for: $name : $ref </span> <br><br>";
 
     if (!mysqli_query($conne, $logLedgerMonth) or !mysqli_query($conne, $updateMonthLog)) {
       die('Error: ' . mysqli_error($conne));
@@ -80,29 +74,24 @@ while ($rowMonths = mysqli_fetch_array($holderMonths)) {
   }
 
   //weekly
-  if ($frequency == "Every Week" and ($dateDay == $nextLog)) {
+  else if ($frequency == "Every Week" and ($dateDay == $nextLog)) {
     $logLedgerWeek = "INSERT INTO ledger (username, ref, cost, dateEntered) VALUES ('$username', '$ref', '$cost', '$todayDate')";
-
-    echo "<span style='color:red'>Logged Week for: $name : $ref </span> <br>";
-
-    //define next log within month scope
+    //define next log within current month scope
     if ($nextLog <= 28) {
       $newNextLog = $nextLog + 7;
-      echo "Next log is plus six: $ref : $newNextLog <br>";
     } else {
-      //reset to normal date
       $newNextLog = $date;
-      echo "Next log reset to sub date for: $ref : $newNextLog <br>";
     }
-
     $updateWeekLog = "UPDATE subs SET nextLog='$newNextLog', lastBilled='$todayDate'  WHERE ref = '$ref'";
-    //send email
-    sendMail("Week");
+    echo "<span style='color:red'>Logged Week for: $name : $ref </span> <br> Next log re-set to sub date for: $ref : $newNextLog <br>";
 
     if (!mysqli_query($conne, $updateWeekLog) or !mysqli_query($conne, $logLedgerWeek)) {
       die('Error: ' . mysqli_error($conne));
     }
+  } else {
+    echo "No week or month sub found!";
   }
+
   echo "Completed log review on: $name <br><br>";
 }
 
