c563785cd7ac1bfe6ae42fe663252d978d58f6e1
chrisenitan <enitanchris@gmail.com>
2021-12-05 17:14:43 +0100

    updates: better db conn mgm and index log

--- connectDb.js
+++ connectDb.js
@@ -8,6 +8,15 @@ if (process.env.appEnvironment == "production") {
     password: process.env.awspass,
     database: process.env.awsdb,
   })
+} else if (process.env.appEnvironment == "fh") {
+  var sqldb = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.fhserver,
+    port: process.env.fhport,
+    user: process.env.fhuser,
+    password: process.env.fhpassword,
+    database: process.env.fhdb,
+  })
 } else {
   var sqldb = mysql.createPool({
     connectionLimit: 10,
@@ -21,12 +30,17 @@ if (process.env.appEnvironment == "production") {
 
 sqldb.getConnection((err, connection) => {
   if (err) {
-    if (err.code === "PROTOCOL_CONNECTION_LOST") console.error("Database connection was closed.")
-    if (err.code === "ER_CON_COUNT_ERROR") console.error("Database has too many connections.")
-    if (err.code === "ECONNREFUSED") console.error("Database connection was refused.")
+    if (
+      err.code === "PROTOCOL_CONNECTION_LOST" ||
+      err.code === "ECONNREFUSED" ||
+      err.code === "ER_CON_COUNT_ERROR" ||
+      err.code === "PROTOCOL_SEQUENCE_TIMEOUT"
+    )
+      console.error(`Database connection was closed. Error: ${err.code}`)
+    else console.log(err)
   } else {
     console.log(
-      `\x1b[34mEnvironment:\x1b[0m ${process.env.appEnvironment}\n\x1b[34mEndpoint:\x1b[0m ${sqldb.config.connectionConfig.host} \n\x1b[34mThread:\x1b[0m ${connection.threadId}`
+      `Database Connected \n\x1b[34mEnvironment:\x1b[0m ${process.env.appEnvironment}\n\x1b[34mEndpoint:\x1b[0m ${sqldb.config.connectionConfig.host} \n\x1b[34mThread:\x1b[0m ${connection.threadId}`
     )
   }
   if (connection) connection.release()
